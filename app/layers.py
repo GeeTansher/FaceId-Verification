0a1d40b3b8b9c7d1841b9b2fd45961d8621837e3
Geetansh Verma <vrgpverma@gmail.com>
2022-05-29 15:01:50 +0530

    Jupyter File and Kivy App File

---
+++ app/layers.py
@@ -0,0 +1,18 @@
+# Custom L1 Distance layer module
+
+# Dependencies
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+# Custom L1 Distance Layer from Jupyter
+
+# Siamese L1 Distance class
+class L1Dist(Layer):
+    
+    # Init method - inheritance
+    def __init__(self, **kwargs):
+        super().__init__()
+       
+    # Magic happens here - similarity calculation
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding)
